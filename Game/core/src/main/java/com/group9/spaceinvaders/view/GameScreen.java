e93129c8595cdb0a9e62cef0b3e9a3da34fe6844
spineli8456 <matheus.spineli@hotmail.com>
2026-05-26 11:32:28 -0300

    Vetorização dos objetos, a fim de facilitar a implentaçao de objetos repetidos

--- Game/core/src/main/java/com/group9/spaceinvaders/view/GameScreen.java
+++ Game/core/src/main/java/com/group9/spaceinvaders/view/GameScreen.java
@@ -1,4 +1,6 @@
 package com.group9.spaceinvaders.view;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
@@ -24,47 +26,51 @@ public class GameScreen extends ScreenAdapter {
     private Player playerOne;
     private PlayerController playerOneController;
 
-    // 2. Declarando o Swarm e seu Controller
-    private Swarm swarm;
-    private SwarmController swarmController;  
-    private PlayerBullet playerBullet;
-    private PlayerBulletController playerBulletController;
-    private EnemyBullet enemyBullet;
-    private EnemyBulletController enemyBulletController;
+    // 1. Declarando as listas que terao os objetos
+    
+    private List<Swarm> swarms = new ArrayList<>(); 
+    private List<SwarmController> swarmControllers = new ArrayList<>();  
+    private List<PlayerBullet> playerBullets = new ArrayList<>();
+    private List<PlayerBulletController> playerBulletControllers = new ArrayList<>();
+    private List<EnemyBullet> enemyBullets = new ArrayList<>();
+    private List<EnemyBulletController> enemyBulletControllers = new ArrayList<>();
     private ShapeRenderer shapeRenderer;
   
     public GameScreen() {
         playerOne = new Player(300, 50, 50, 50);
-        playerBullet = new PlayerBullet(-10, -10, 5, 15);
-        enemyBullet = new EnemyBullet(-10, -10, 5, 15);
+        playerBullets.add(new PlayerBullet(-10, -10, 5, 15));
+        for(int i = 0; i < 5; i++){
+            enemyBullets.add(new EnemyBullet(-10, -10, 5, 15));
+        }
         playerOneController = new PlayerController(playerOne);
 
         // 3. Inicializa a nuvem de inimigos
         // Parâmetros: startX, startY, width, height, padding (espaçamento)
         // Coloquei Y = 400 para eles começarem na parte de cima da tela
-        swarm = new Swarm(50, 400, 30, 30, 15);
+        swarms.add(new Swarm(50, 400, 30, 30, 15));
         
         // O Controller precisa da largura da tela. 
         // O Gdx.graphics.getWidth() pega o tamanho exato da janela do seu jogo!
-        swarmController = new SwarmController(swarm, Gdx.graphics.getWidth());
-        playerBulletController = new PlayerBulletController(playerBullet);
-        enemyBulletController = new EnemyBulletController(enemyBullet);
-        
+        swarmControllers.add(new SwarmController(swarms.get(0), Gdx.graphics.getWidth()));
+        playerBulletControllers.add(new PlayerBulletController(playerBullets.get(0)));
+        for(int i = 0; i < 5; i++){
+            enemyBulletControllers.add(new EnemyBulletController(enemyBullets.get(i)));
+        }
         shapeRenderer = new ShapeRenderer();
     }
 
     @Override
     public void render(float delta) {
         // --- 4. FASE DE ATUALIZAÇÃO LÓGICA (CONTROLLERS) ---
-        playerOneController.update(delta, enemyBullet); // Lê o teclado e move a nave
-        swarmController.update(delta, playerBullet); // Faz o zigue-zague matemático acontecer
+        playerOneController.update(delta, enemyBullets.get(0)); // Lê o teclado e move a nave
+        swarmControllers.get(0).update(delta, playerBullets.get(0)); // Faz o zigue-zague matemático acontecer
         
-        playerBulletController.PlayerShootUpdate(playerOne);
-        playerBulletController.update(delta);
-        
-        enemyBulletController.EnemyShootUpdate(swarm);
-        enemyBulletController.update(delta);
-        
+        playerBulletControllers.get(0).PlayerShootUpdate(playerOne);
+        playerBulletControllers.get(0).update(delta);
+        for(int i = 0; i < 5; i++){
+            enemyBulletControllers.get(i).EnemyShootUpdate(swarms.get(0));
+            enemyBulletControllers.get(i).update(delta);
+        }
 
         // --- 5. FASE DE DESENHO (VIEW) ---
         ScreenUtils.clear(0, 0, 0, 1);
@@ -79,10 +85,9 @@ public class GameScreen extends ScreenAdapter {
         shapeRenderer.setColor(Color.RED);
         
         // Percorre a nossa matriz 5x11
-        for (int r = 0; r < swarm.rows; r++) {
-            for (int c = 0; c < swarm.cols; c++) {
-                Enemy enemy = swarm.enemies[r][c];
-                
+        for (int r = 0; r < swarms.get(0).rows; r++) {
+            for (int c = 0; c < swarms.get(0).cols; c++) {
+                Enemy enemy = swarms.get(0).enemies[r][c];
                 // Só manda a placa de vídeo desenhar se o inimigo ainda estiver vivo
                 if (enemy.isAlive) {
                     shapeRenderer.rect(enemy.bounds.x, enemy.bounds.y, enemy.bounds.width, enemy.bounds.height);
@@ -91,16 +96,18 @@ public class GameScreen extends ScreenAdapter {
         }
         // Desenha o Tiro do Inimigo (Vermelho)
         shapeRenderer.setColor(Color.RED);
-        if(enemyBullet.isValid){
-        shapeRenderer.rect(enemyBullet.bounds.x, enemyBullet.bounds.y, enemyBullet.bounds.width, enemyBullet.bounds.height);
+        for(int i = 0; i < 5; i++){
+            if(enemyBullets.get(i).isValid){
+                shapeRenderer.rect(enemyBullets.get(i).bounds.x, enemyBullets.get(i).bounds.y, enemyBullets.get(i).bounds.width, enemyBullets.get(i).bounds.height);
+            }
         }
 
         // Desenha o Tiro (Branco)
         shapeRenderer.setColor(Color.WHITE);
         
-        if(playerBullet.isValid){
+        if(playerBullets.get(0).isValid){
             playerOne.canShoot = false;
-            shapeRenderer.rect(playerBullet.bounds.x, playerBullet.bounds.y, playerBullet.bounds.width, playerBullet.bounds.height);
+            shapeRenderer.rect(playerBullets.get(0).bounds.x, playerBullets.get(0).bounds.y, playerBullets.get(0).bounds.width, playerBullets.get(0).bounds.height);
         } else {
             playerOne.canShoot = true;
         }
